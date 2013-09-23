0b4ab8414e4529499e590837b2ee26fc972e0911
Marc Aldorasi <m101010a@gmail.com>
2013-09-23 05:30:25 -0400

    Added xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,108 @@
+import XMonad
+import Graphics.X11.ExtraTypes.XF86
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.CycleWS
+import XMonad.Actions.KeyRemap
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.NoBorders
+import XMonad.ManageHook
+import XMonad.Util.Loggers
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import System.IO
+
+import qualified XMonad.StackSet as W
+
+-- Have a convenient way to sink windows in my manage hook
+doSink = (ask >>= doF . W.sink)
+
+-- Double carets so dzen doesn't get confused when they're in window titles
+doubleCarets [] = []
+doubleCarets ('^':cs) = '^':'^':(doubleCarets cs)
+doubleCarets (c:cs) = c:(doubleCarets cs)
+
+-- Leave space for trayer and the other dzen (my screen is 1600 wide)
+dzenCommand = "dzen2 -e 'button2=;' -x 250 -h 16 -w 1100"
+
+-- Put all my IMs on desktop 8 and prevent xmonad recompilation errors from
+-- resizing my windows, plus tile/float my games
+myManageHook = composeAll [
+	isDialog --> doCenterFloat,
+	className =? "Xmessage" --> doFloat,
+	className =? "Pidgin" --> doShift "8",
+	title =? "Jamestown" --> doFloat,
+	title =? "osu!" --> doSink,
+	manageDocks]
+
+-- The default plus an option to center the current window instead of
+-- stretching it.  Then don't draw borders if there's only 1 window on the
+-- screen and try not to overlap my statusbar
+myLayoutHook = avoidStruts (smartBorders tall ||| smartBorders (Mirror tall) ||| smartBorders Full ||| (layoutHintsToCenter $ smartBorders Full))
+	where tall = Tall 1 (3/100) (1/2)
+
+-- Make dzen have nice colors
+myPP h = defaultPP {
+	-- Selected workspace
+	ppCurrent = wrap "^fg(#77d7d7)" "^fg()",
+	-- Deselected workspaces
+	ppVisible = wrap "^fg(#777777)" "^fg()",
+	ppHidden = wrap "^fg(#777777)" "^fg()",
+	-- Something requires my attention
+	ppUrgent = wrap "^fg(#f0b77f)" "^fg()",
+	-- Current window title
+	ppTitle = wrap "^fg(white)" "^fg()" . doubleCarets,
+	ppSep = " - ",
+	ppWsSep = " ",
+	ppOutput = hPutStrLn h
+	}
+
+dzenLogHook = dynamicLogWithPP . myPP
+
+-- Adds the keys in y to x's config, but let y depend on x
+addKeys x y = additionalKeys x (y x)
+
+myKeys conf =
+	-- Lock the screen
+	[((mod4Mask .|. shiftMask, xK_z), unsafeSpawn "xscreensaver-command -lock && xset s activate"),
+	-- Start firefox
+	((mod4Mask, xK_f), safeSpawn "firefox" ["--no-remote"]),
+	-- Take a screenshot
+	((mod4Mask, xK_s), safeSpawn "scrot" ["%Y.%m.%d-%T.png", "-e", "mv -f $f ~"]),
+	-- Take a screenshot of a window
+	((mod4Mask .|. shiftMask, xK_s), safeSpawn "scrot" ["%Y.%m.%d-%T.png", "-s", "-e", "mv -f $f ~"]),
+	-- If there are multiple instances of a window only kill the focused one
+	((mod4Mask .|. shiftMask, xK_c), kill1),
+	-- Make windows overlap dzen and the system tray
+	((mod4Mask, xK_b), sendMessage ToggleStruts),
+	-- Cycle through workspaces
+	((mod4Mask, xK_Right), nextWS),
+	((mod4Mask, xK_Left), prevWS),
+	-- Cycle through workspaces and bring the current window with me
+	((mod4Mask .|. shiftMask, xK_Right), shiftToNext >> nextWS),
+	((mod4Mask .|. shiftMask, xK_Left), shiftToPrev >> prevWS)]
+	++
+	-- Copy the current window to another desktop
+	[(((mod4Mask .|. shiftMask .|. controlMask), k), windows $ copy i)
+		| (i, k) <- zip (workspaces conf) [xK_1 ..]]
+
+-- Highlight urgent windows and enable Extended Hints
+myConfig dzen = withUrgencyHook NoUrgencyHook $ ewmh defaultConfig {
+	terminal = "urxvt",
+	modMask = mod4Mask,
+	logHook = dzenLogHook dzen >> takeTopFocus,
+	layoutHook = myLayoutHook,
+	manageHook = myManageHook
+	}
+	`addKeys`
+	myKeys
+
+main = do
+	dzen <- spawnPipe dzenCommand
+	xmonad $ myConfig dzen
